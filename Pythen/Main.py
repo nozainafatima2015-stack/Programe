6156ca6eafdc0f121626c3256059f5ee5c2e26d9
nozainafatima2015-stack <nozaina.fatima2015@gmail.com>
2025-11-08 08:53:55 +0000

    Main.py

---
+++ Pythen/Main.py
@@ -0,0 +1,18 @@
+name = "bunny"
+age = 15
+grade = 5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("Grade :", grade)
+print("Data type of Grade is", type(grade))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(grade)
+print(grade)
+print("Data Type of grade is", type(grade))

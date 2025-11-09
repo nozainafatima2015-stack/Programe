7327b11b085075f683b5f7eb80796fd52f4cdf9b
bunny7887 <nozaina.fatima2015@email.com>
2025-11-09 15:32:32 +0500

    Number.py

---
+++ Pythen/Numbers.py
@@ -0,0 +1,11 @@
+number = int(input("Eneter number to check"))
+print("Number to be checked:" , number)
+
+if number>0:
+    print("This number is Possitive")
+
+elif number<0:
+        print("This number is Negitive number")
+
+else:
+    print("This is neutral number")

88e9696a19b99a84004b5c4d8e6b77084d1efaf8
bunny7887 <nozaina.fatima2015@email.com>
2025-12-06 15:34:36 +0500

    Denomination.py

---
+++ Pythen Refresher/Denomination.py
@@ -0,0 +1,13 @@
+  
+
+
+Q = [2000 , 500 , 200 , 100 , 50 , 20 , 10]
+X = 0
+
+for i in range(7):
+    q=Q[i]
+    x=a//q
+    print("Notes of {} = {}".formate(q,x))
+
+amount = int(input("Enater the amount :"))
+noofnotes(2500)

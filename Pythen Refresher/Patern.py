f9e66a0fe1c0e4e3856ae4f599328396c7097257
bunny7887 <nozaina.fatima2015@email.com>
2025-12-06 15:06:48 +0500

    Pattern.py

---
+++ Pythen Refresher/Patern.py
@@ -0,0 +1,11 @@
+print("Right Angle pattern /n")
+for i in range(1,6):
+    for J in range(i):
+        print("*",end="")
+    print('/n')
+
+print("Interverded Right Angle pattern /n")
+for i in range(6,1, -1):
+    for J in range(i,1,-1):
+        print("*",end="")
+    print('/n')
